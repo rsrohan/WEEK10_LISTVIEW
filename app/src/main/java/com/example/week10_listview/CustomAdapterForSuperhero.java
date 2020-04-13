4789bb9e3ebbe9d753113be885234d9fbf72d1b5
Rohan Verma <rsrohanverma@gmail.com>
2020-04-13 19:42:57 +0530

    Ques1 done

--- app/src/main/java/com/example/week10_listview/CustomAdapterForSuperhero.java
+++ app/src/main/java/com/example/week10_listview/CustomAdapterForSuperhero.java
@@ -1,6 +1,9 @@
 package com.example.week10_listview;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +11,14 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 
@@ -40,16 +51,39 @@ public class CustomAdapterForSuperhero extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view =  inflater.inflate(R.layout.list_items_for_superheros,parent,false);
 
-        ImageView dp = view.findViewById(R.id.ivSuperhero);
+        final ImageView dp = view.findViewById(R.id.ivSuperhero);
         TextView name = view.findViewById(R.id.tvSuperheroName);
         TextView superpower = view.findViewById(R.id.tvSuperpower);
         Button delete = view.findViewById(R.id.btnDelete);
 
 
+        Glide.with(context).load(Uri.parse(superheroArrayList.get(position).getDp())).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                Log.d("TAG", "onLoadFailed: "+e);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                dp.setImageDrawable(resource);
+                return true;
+            }
+        }).submit();
+        name.setText(superheroArrayList.get(position).getName());
+        superpower.setText(superheroArrayList.get(position).getSuperpower());
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                superheroArrayList.remove(position);
+                notifyDataSetChanged();
+            }
+        });
 
 
         return view;
--- app/src/main/java/com/example/week10_listview/QUES1.java
+++ app/src/main/java/com/example/week10_listview/QUES1.java
@@ -20,7 +20,25 @@ public class QUES1 extends AppCompatActivity {
         lvSuperhero = findViewById(R.id.lvSuperhero);
         superheroArrayList = new ArrayList<>();
 
+        Superhero ironman = new Superhero("https://images.news18.com/optimize/0WKBxfg4iGxKo_11iMYDIz7fMQg=/698x552/images.news18.com/ibnlive/uploads/698x552/jpg/2016/04/ironman.jpg", "Iron Man", "He is Iron Man");
+        Superhero captainAmerica = new Superhero("https://i.pinimg.com/originals/d5/81/79/d58179ac3e5781037d0fbf2bf3a97cdc.jpg", "Captain America", "Shield");
+        Superhero spiderman = new Superhero("https://upload.wikimedia.org/wikipedia/en/thumb/2/21/Web_of_Spider-Man_Vol_1_129-1.png/250px-Web_of_Spider-Man_Vol_1_129-1.png", "Spider Man", "His uncle is Iron Man");
+        Superhero hulk = new Superhero("https://img.etimg.com/thumb/msid-72400227,width-1200,height-900,imgsize-349917,overlay-etpanache/photo.jpg", "Hulk", "Very Humble");
+        Superhero thor = new Superhero("https://image.cnbcfm.com/api/v1/image/104819285-thor.jpg?v=1529476684&w=678&h=381", "Thor", "God of Thunder");
+        Superhero natasha = new Superhero("https://i.ytimg.com/vi/KLBmMLCovlc/maxresdefault.jpg", "Natasha", "She can do everything");
+        Superhero blackPanther = new Superhero("https://media.newyorker.com/photos/5a875e3f33aebd0cab9bab12/4:3/w_1439,h_1079,c_limit/Brody-Passionate-Politics-Black-Panther.jpg", "Black Panther", "Quarantined Wakanda before Corona");
 
+
+        superheroArrayList.add(ironman);
+        superheroArrayList.add(captainAmerica);
+        superheroArrayList.add(spiderman);
+        superheroArrayList.add(hulk);
+        superheroArrayList.add(thor);
+        superheroArrayList.add(natasha);
+        superheroArrayList.add(blackPanther);
+
+        CustomAdapterForSuperhero customAdapterForSuperhero = new CustomAdapterForSuperhero(getApplicationContext(), superheroArrayList);
+        lvSuperhero.setAdapter(customAdapterForSuperhero);
 
     }
 }
